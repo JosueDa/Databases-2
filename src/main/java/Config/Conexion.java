0469891ddf2a55d623f9977d66ed8a931e346f05
JosueDa <morales181101@unis.edu.gt>
2021-08-30 17:33:38 -0600

    changing database name

--- src/main/java/Config/Conexion.java
+++ src/main/java/Config/Conexion.java
@@ -17,7 +17,7 @@ public class Conexion {
 
         try {
               Class.forName("com.mysql.jdbc.Driver");
-              con=DriverManager.getConnection("jdbc:mysql://192.168.1.5:3306/system?","root","example");
+              con=DriverManager.getConnection("jdbc:mysql://192.168.1.5:3306/dev?","root","example");
         } catch (Exception e) {
             
         }
