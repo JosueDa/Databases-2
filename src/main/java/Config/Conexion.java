4ae245ac2e5c3135cda708bba6577d0acfc1f787
JosueDa <morales181101@unis.edu.gt>
2021-09-14 17:25:24 -0600

    test 2

--- src/main/java/Config/Conexion.java
+++ src/main/java/Config/Conexion.java
@@ -18,7 +18,7 @@ public class Conexion {
         try {
               Class.forName("com.mysql.jdbc.Driver");
 
-              con=DriverManager.getConnection("jdbc:mysql://192.168.1.5:3306/main?","root","example");
+              con=DriverManager.getConnection("jdbc:mysql://192.168.1.5:3306/QA?","root","example");
         } catch (Exception e) {
             
         }
